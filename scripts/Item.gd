50557796247ac85a0cae14e0c7b11ea0637418d2
jareddlowe <jareddlowe@gmail.com>
2023-05-20 18:12:30 -0400

    Take and drop function

--- scripts/Item.gd
+++ scripts/Item.gd
@@ -9,7 +9,6 @@ var item_name : String
 var use_function : Callable
 var type
 
-# Item is being deleted and recreated every frame
 
 func _ready():
 	print('Item created!')
@@ -28,44 +27,63 @@ func _input(event):
 				if slot in main.get_node("%InventoryGrid").get_children():
 					use()
 				elif slot in main.get_node("%NearbyItemsGrid").get_children():
-					move_to_inventory(self)
+					move_to_inventory()
 	
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_RIGHT:
 			if main.check_if_mouse_above(self) and not inv_manager.picked_item:
+				if is_instance_valid(main.rc_menu):
+					main.rc_menu.queue_free()
+				# Instantiate a new right click menu
+				var new_menu = load("res://scenes/RightClickMenu.tscn").instantiate()
+				main.rc_menu = new_menu
+				new_menu.global_position = get_global_mouse_position()
+				main.add_child(new_menu)
 				if slot in main.get_node("%InventoryGrid").get_children():
-					if is_instance_valid(main.rc_menu):
-						main.rc_menu.queue_free()
-					# Instantiate a new right click menu
-					var new_menu = load("res://scenes/RightClickMenu.tscn").instantiate()
-					main.rc_menu = new_menu
-					new_menu.global_position = get_global_mouse_position()
-					main.add_child(new_menu)
-					# Add a drop button and give drop function to it
+					# Add a drop button and connect its pressed signal to our drop function 
 					var new_button = load("res://scenes/RightClickMenuButton.tscn").instantiate()
 					new_button.text = "Drop"
-					new_button.button_down.connect(_drop)
+					new_button.pressed.connect(drop)
 					new_menu.get_node("PanelContainer/VBoxContainer").add_child(new_button)
 					
+				if slot in main.get_node("%NearbyItemsGrid").get_children():
+					# Add a take button and connect its pressed signal to our take function
+					var new_button = load("res://scenes/RightClickMenuButton.tscn").instantiate()
+					new_button.text = "Take"
+					new_button.pressed.connect(take)
+					new_menu.get_node("PanelContainer/VBoxContainer").add_child(new_button)
+					
+				# Add a cancel button
+				var new_button = load("res://scenes/RightClickMenuButton.tscn").instantiate()
+				new_button.text = "Cancel"
+				new_button.pressed.connect(cancel)
+				new_menu.get_node("PanelContainer/VBoxContainer").add_child(new_button)
+
 
 func use():
 	if type == "Buriable":
 		main.player.inventory.slots[slot.get_index()].item = null
 
 
-func _drop():
-	print('ye')
+func drop():
 	if main.rc_menu:
 		main.rc_menu.queue_free()
 	inv_manager.add_item(resource, main.player.location.inventory)
 	main.player.inventory.slots[slot.get_index()].item = null
-	
 
 
-func move_to_inventory(item):
-	var to_slot = item.slot
-	main.player.location.inventory.slots[to_slot.get_index()].item = null
-	#item.get_parent().remove_child(item)
-	# first empty slot
-	main.get_node("InventoryManager").add_item(item.resource, main.player.inventory)
+func take():
+	if main.rc_menu:
+		main.rc_menu.queue_free()
+	move_to_inventory()
 
+
+func move_to_inventory():
+	var item = main.player.location.inventory.slots[slot.get_index()].item
+	main.player.location.inventory.slots[slot.get_index()].item = null
+	inv_manager.add_item(item, main.player.inventory)
+
+
+func cancel():
+	if main.rc_menu:
+		main.rc_menu.queue_free()
